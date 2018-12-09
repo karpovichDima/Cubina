12bd7a1772238cfe17758f575b8c92676b7d3983
Karpovich Dima <karpovichdi@hotmail.com>
2018-12-09 21:20:14 +0300

    added moving to forward, left, right

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 500f;
+    public float sidewayForce = 500f;
+
+	void FixedUpdate () {
+
+	    rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+	    {
+	        rb.AddForce(sidewayForce * Time.deltaTime, 0, 0 );
+        }
+
+	    if (Input.GetKey("a"))
+	    {
+	        rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+	    }
+    }
+}
