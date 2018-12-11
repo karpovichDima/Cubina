2cf41bb7a32def772a4125dd84d6c90e5f7c9b0b
Karpovich Dima <karpovichdi@hotmail.com>
2018-12-11 23:37:00 +0300

    Added scripts for collision and restart, new material for obstacles and new position obstacles

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Transform _userPosition;
+    public Rigidbody _userRigitBody;
+    private Thread thread;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Handheld.Vibrate();
+            for (int i = 0; i < 6; i++)
+            {
+                _userPosition.SetPositionAndRotation(new Vector3(_userPosition.position.x, _userPosition.position.y, 
+                    _userPosition.position.z - 0.5f), _userPosition.rotation);
+            }
+        }  
+    }
+}
---
+++ Assets/PlayerRestart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerRestart : MonoBehaviour {
+
+    public Transform _userPosition;
+
+	void Update () {
+	    if (_userPosition.position.z >= 60)
+	    {
+	       _userPosition.SetPositionAndRotation(new Vector3(_userPosition.position.x, _userPosition.position.y,
+	           _userPosition.position.z - 60), _userPosition.rotation);
+        }
+	}
+}
