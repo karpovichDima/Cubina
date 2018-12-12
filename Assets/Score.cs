d106f6382eddc6affe72b4428f220a3517aa0242
Karpovich Dima <karpovichdi@hotmail.com>
2018-12-12 23:01:21 +0300

    add to UI text Score

---
+++ Assets/Score.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+
+	void Update ()
+	{
+	    scoreText.text = Convert.ToInt64(player.position.z).ToString();
+	}
+}
