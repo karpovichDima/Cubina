f52bd7610504d268f6112fa966f3179c0dcc7c50
Karpovich Dima <karpovichdi@hotmail.com>
2018-12-12 23:17:34 +0300

    Added mass for little cube

--- Assets/Scripts/PlayerRestart.cs
+++ Assets/Scripts/PlayerRestart.cs
@@ -5,10 +5,14 @@ public class PlayerRestart : MonoBehaviour {
     public Transform _userPosition;
 
 	void Update () {
-	    if (_userPosition.position.z >= 60)
+	    if (_userPosition.position.z >= 150)
 	    {
 	       _userPosition.SetPositionAndRotation(new Vector3(_userPosition.position.x, _userPosition.position.y,
-	           _userPosition.position.z - 60), _userPosition.rotation);
+	           0), _userPosition.rotation);
+        } else if (_userPosition.position.y <= 0)
+	    {
+	       _userPosition.SetPositionAndRotation(new Vector3(_userPosition.position.x, 0.8f,
+	            0), _userPosition.rotation);
         }
 	}
 }
